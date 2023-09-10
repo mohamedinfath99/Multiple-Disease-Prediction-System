2e6f109a815a98be153a857792c3d6cfe4c348f9
Rafeek Mohamed Infath <mohamedinfath99@gmail.com>
2023-09-10 13:50:27 +0530

    code added

--- disease-app/src/pages/Contact.jsx
+++ disease-app/src/pages/Contact.jsx
@@ -2,9 +2,48 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
-            contact
-        </div>
+        <section>
+            <div className='px-4 mx-auto max-w-screen-md'>
+                <h2 className='heading text-center'>Contact Us</h2>
+                <p className='mb-8 lg:mb-16 font-light text-center text__para'>Got a technical issue? Want to send feedback about a beta features? Let us know</p>
+
+                <form>
+                    <div>
+                        <label htmlFor='email' className='form__label'>Your Email</label>
+                        <input
+                            type='email'
+                            id='email'
+                            placeholder='example@gmail.com'
+                            className='form__input mt-1'
+                        />
+                    </div>
+
+
+                    <div className='mt-3'>
+                        <label htmlFor='subject' className='form__label'>Subject</label>
+                        <input
+                            type='text'
+                            id='subject'
+                            placeholder='Let us know how we can help you'
+                            className='form__input mt-1'
+                        />
+                    </div>
+
+
+                    <div className='mt-3 sm:col-span-2'>
+                        <label htmlFor='message' className='form__label'>Your Message</label>
+                        <textarea
+                            type='text'
+                            id='message'
+                            placeholder='Leave a comment...'
+                            className='form__input mt-1'
+                        />
+                    </div>
+
+                    <button type='submit' className='btn rounded sm:w-fit'>Submit</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
